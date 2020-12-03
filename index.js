f952327341084160af48b9cd68277044a1697a74
Newland-A <ajn252@gmail.com>
2020-12-02 19:36:36 -0500

    commit tree 5db077be4d424a3de8c5e7adeabd97c386b503b1

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// [1,3,5,6,8].find(function(e) { return e % 2 === 0 })
+// => 6
+
+function superbowlWin(record) {
+  let results = record.find(record => record.result === "W")
+  return !!results ? results.year : undefined
+} 
+superbowlWin()
