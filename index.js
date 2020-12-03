4f73b2e6de8100cee7ad71e0ab8c0383f4819d32
Newland-A <ajn252@gmail.com>
2020-12-02 19:38:03 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// [1,3,5,6,8].find(function(e) { return e % 2 === 0 })
+// => 6
+
+function superbowlWin(record) {
+  let results = record.find(record => record.result === "W")
+  return !!results ? results.year : undefined
+} 
+
+// or this is also a function 
+
+// superbowlWin = (record) => {
+//   let results = record.find( record => record.result === "W" )
+//   return !!results ? results.year : undefined
+// }
+superbowlWin() //then gets invoked
